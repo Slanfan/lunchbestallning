c2fd7f37bcb1e12cc9561dc3290c1cc880106ef4
Slanfan <slanfan@gmail.com>
2015-09-06 16:03:51 +0200

    update

--- js/login.js
+++ js/login.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function() {
 	if(localStorage.getItem("username") != null) {
-		alert("auto login/n" + localStorage.getItem("username"));
+		alert("auto login (" + localStorage.getItem("username") + ")");
+		$('#login').hide();
 	} else {
 		alert("need login");
 	}
@@ -9,14 +10,20 @@ $(document).ready(function() {
 
 function login() {
 
+	// get input information
 	var username = $('#username').val();
     var password = $('#password').val();
 
+    // store user information locally
     localStorage.setItem("username", username);
 
     PushbotsPlugin.setAlias(username);
     //PushbotsPlugin.resetBadge();
     console.log(PushbotsPlugin.getToken());
+}
 
+function logout() {
 
+	// remove stores userinformation
+	localStorage.removeItem("username");
 }
