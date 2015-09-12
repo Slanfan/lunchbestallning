eaa700a5316427a2923623e11ff5d0a1de8759cc
Slanfan <slanfan@gmail.com>
2015-09-12 20:06:39 +0200

    asd

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,19 @@ function load_menu() {
 	var url = 'http://www.lunchbestallning.se/app/todays-menu.php?company=' + localStorage.getItem("company");
 
 	// load data from url and add slick slider to menu-slider
-	/*
-	$('#todays-menu').load(url, function(){
+	$.get(url, function(data){
+		console.log(data);
+		$('#todays-menu').append(data);
 		$('#todays-menu').slick({
 			arrows: false,
 			dots: true
 	  	});
 	});
-	*/
+	/*
 	$('#todays-menu').append('<div class="menu-item"><div class="menu-item-container"><div class="image-wrapper"><img src="img/menu-icons/menu-fisk.png" /></div><p>Fisk och skaldjursgryta med aioli och krutonger</p></div></div><div class="menu-item"><div class="menu-item-container"><div class="image-wrapper"><img src="img/menu-icons/menu-soppa.png" /></div><p>Ärtsoppa med fläsk samt pannkakor</p></div></div><div class="menu-item"><div class="menu-item-container"><div class="image-wrapper"><img src="img/menu-icons/menu-vilt.png" /></div><p>Viltskav med potatispuré och svartvinbärsgelé</p></div></div>');
 	$('#todays-menu').slick({
 		arrows: false,
 		dots: true
   	});
+  	*/
 }
