849696bc23003d1d3a02686cdfb31e8b757b9f37
Slanfan <slanfan@gmail.com>
2015-09-15 23:54:19 +0200

    asd

--- js/scripts.js
+++ js/scripts.js
@@ -38,10 +38,10 @@ function toggle_settings() {
 	}
 }
 function load_user_data() {
-    $('#employee-company').html(localStorage.getItem("company"));
-    $('#employee-number').html(localStorage.getItem("employee-number"));
-    $('#employee-name').html(localStorage.getItem("employee-name"));
-    $('#employee-email').html(localStorage.getItem("employee-email"));
+    $('#employee-company').text(localStorage.getItem("company"));
+    $('#employee-number').text(localStorage.getItem("employee-number"));
+    $('#employee-name').text(localStorage.getItem("employee-name"));
+    $('#employee-email').text(localStorage.getItem("employee-email"));
 }
 function load_menu() {
 	// store menu-url to load in variable
